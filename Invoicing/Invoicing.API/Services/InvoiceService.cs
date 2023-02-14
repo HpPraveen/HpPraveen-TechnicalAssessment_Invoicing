022613c357f9713077fbf4f07153c2e7d6e26d33
Praveen Howpage <pHowpage@braveenergy.com.au>
2023-02-14 19:00:59 +0530

    create InvoiceService

---
+++ Invoicing/Invoicing.API/Services/InvoiceService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Invoicing.API.Services.Interfaces;
+using Invoicing.Domain.DTO;
+using Invoicing.Infrastructure.UnitOfWork;
+
+namespace Invoicing.API.Services
+{
+    internal class InvoiceService : IInvoiceService
+    {
+        private readonly IGenericUnitOfWork _genericUnitOfWork;
+        private readonly IMapper _mapper;
+
+        public InvoiceService(IGenericUnitOfWork genericUnitOfWork, IMapper mapper)
+        {
+            _genericUnitOfWork = genericUnitOfWork;
+            _mapper = mapper;
+        }
+
+        public object CreateUpdateInvoice(InvoiceDto invoiceDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteInvoice(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetInvoiceById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetInvoices(int pageNumber, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
