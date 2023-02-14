a36ab73a304199809d00b70a498ebc5343474e21
Praveen Howpage <pHowpage@braveenergy.com.au>
2023-02-14 18:47:55 +0530

    create generic Repo and UOM

---
+++ Invoicing/Invoicing.Infrastructure/UnitOfWork/GenericUnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using Invoicing.Domain;
+using Invoicing.Infrastructure.Repository;
+using static System.GC;
+
+namespace Invoicing.Infrastructure.UnitOfWork
+{
+    public class GenericUnitOfWork : IGenericUnitOfWork
+    {
+        private readonly CosmosDbContext _context;
+        private bool _disposed;
+
+        public GenericUnitOfWork(CosmosDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _context.Dispose();
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            SuppressFinalize(this);
+        }
+
+        public GenericRepository<Invoice> InvoiceRepository => new(_context);
+    }
+}
---
+++ Invoicing/Invoicing.Infrastructure/UnitOfWork/IGenericUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Invoicing.Domain;
+using Invoicing.Infrastructure.Repository;
+
+namespace Invoicing.Infrastructure.UnitOfWork
+{
+    public interface IGenericUnitOfWork
+    {
+        public Task CommitAsync();
+
+        public void Commit();
+
+        public void Dispose();
+
+        public GenericRepository<Invoice> InvoiceRepository { get; }
+    }
+}
